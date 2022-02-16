a650e531ebcd4a9b942ad29d42290eeaa5e52da2
Roberval Filho <rslfilho@gmail.com>
2022-02-16 18:31:27 -0300

    feat : adicionando erros de login e autenticação

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -14,6 +14,26 @@ const errors = {
     code: 'not_found',
     message: 'Task does not exist',
   },
+  userNotFound: {
+    statusCode: 404,
+    code: 'not_found',
+    message: 'User does not exist',
+  },
+  wrongPassword: {
+    statusCode: 400,
+    code: 'bad_request',
+    message: '"password" does not match',
+  },
+  missingToken: {
+    statusCode: 401,
+    code: 'unauthorized',
+    message: 'Token not found',
+  },
+  jwtMalformed: {
+    statusCode: 401,
+    code: 'unauthorized',
+    message: 'Expired or invalid token',
+  },
 };
 
 module.exports = errors;
