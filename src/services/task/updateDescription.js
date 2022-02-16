8b54e056b51edf38d308c4d886825e3cfb96de60
Roberval Filho <rslfilho@gmail.com>
2022-02-16 18:10:27 -0300

    fix : atualizando serviço para mostrar quando não houve atualização

--- src/services/task/updateDescription.js
+++ src/services/task/updateDescription.js
@@ -4,7 +4,7 @@ const { errors } = require('../../helpers');
 module.exports = async (email, taskId, { description }) => {
   const count = await taskModel.updateDescription(email, taskId, { description });
 
-  if (!count) throw errors.taskNotFound;
+  if (!count && count !== 0) throw errors.taskNotFound;
 
   return { message: `${count} task updated` };
 };
