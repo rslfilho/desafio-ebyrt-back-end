c6fd42d124638df478294f23d54e7583398df47a
Roberval Filho <rslfilho@gmail.com>
2022-02-16 18:32:30 -0300

    feat : middleware de validação de login

--- src/middlewares/validation/index.js
+++ src/middlewares/validation/index.js
@@ -1,9 +1,11 @@
 const user = require('./user');
 const description = require('./description');
 const status = require('./status');
+const login = require('./login');
 
 module.exports = {
   user,
   description,
   status,
+  login,
 };
