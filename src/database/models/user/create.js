4b180392894dc8e338c758ca8b014a01ad1ed322
Roberval Filho <rslfilho@gmail.com>
2022-02-16 15:49:39 -0300

    feat : model de criação de usuário

---
+++ src/database/models/user/create.js
@@ -0,0 +1,20 @@
+const connection = require('../../connection');
+
+module.exports = async ({ firstName, lastName, email, password }) => {
+  const db = await connection();
+  const collection = await db.collection('users');
+  const { insertedId: _id } = await collection.insertOne({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+
+  return {
+    _id,
+    firstName,
+    lastName,
+    email,
+    password,
+  };
+};
