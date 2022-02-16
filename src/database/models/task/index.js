932feefd33a335f48cd477f4fc5257d040090d1c
Roberval Filho <rslfilho@gmail.com>
2022-02-16 18:11:14 -0300

    feat : model para atualizar status de tarefa

--- src/database/models/task/index.js
+++ src/database/models/task/index.js
@@ -1,7 +1,9 @@
 const create = require('./create');
 const updateDescription = require('./updateDescription');
+const updateStatus = require('./updateStatus');
 
 module.exports = {
   create,
   updateDescription,
+  updateStatus,
 };
