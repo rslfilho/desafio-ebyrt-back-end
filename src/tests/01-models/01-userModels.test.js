e97c503a2cd7c119fa3d16fd47402ddfc7a0ecb9
Roberval Filho <rslfilho@gmail.com>
2022-02-17 15:21:35 -0300

    [ PR ] - Adicionando testes de models (#8)
    * setup : instalando dependências de testes
    
    * setup : instalando mongodb-memory-server
    
    * setup : adicionando script de test
    
    * test : criando mock de conexão com o banco em memória
    
    * test : adicionando primeiros mocks
    
    * test : testes de Models de usuário
    
    * refac : forma de guardar o Id do usuário criado
    
    * test : testes dos models de tarefas
    
    * setup : configurando nyc para relatório de cobertura de testes
    
    * fix : tirando .only de teste de tarefa
    
    * action : adicionando action de teste

---
+++ src/tests/01-models/01-userModels.test.js
@@ -0,0 +1,59 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const mongoDBMock = require('../connectionMock');
+const userModel = require('../../database/models/user');
+const dbMock = require('../mocks/dbMock');
+
+describe('Testando os Models de Usuário', () => {
+  let connectionMock;
+  let usersCollection;
+  let userId;
+
+  before(async () => {
+    connectionMock = await mongoDBMock.connection();
+
+    sinon.stub(MongoClient, 'connect')
+      .resolves(connectionMock);
+    
+    const db = await connectionMock.db('to-do-app-test');
+    usersCollection = await db.collection('users');
+
+    const { insertedId } = await usersCollection.insertOne(dbMock.user);
+    userId = insertedId;
+  });
+
+  after(async () => {
+    await MongoClient.connect.restore();
+    await usersCollection.deleteMany({});
+  });
+
+  describe('O model "findByEmail" de user', () => {
+    it('retorna nulo quando o email não existe no banco de dados', async () => {
+      const response = await userModel.findByEmail('email@errado.com');
+      expect(response).to.be.null;
+    });
+  
+    it('retorna o usuário quando o email existe no banco de dados', async () => {
+      const response = await userModel.findByEmail(dbMock.email);
+      expect(response).to.deep.equal({ _id: ObjectId(userId), ...dbMock.user });
+    });
+  });
+
+  describe('O model "create" de user', () => {
+    let response;
+
+    before(async () => {
+      response = await userModel.create(dbMock.newUser);  
+    });
+
+    it('retorna um objeto', () => {
+      expect(response).to.be.an('object');
+    });
+
+    it('retorna o usuário sem a senha e com _id', () => {
+      expect(response).to.have.all.keys('_id', 'firstName', 'lastName', 'email');
+    });
+  });
+});
---
+++ src/tests/mocks/dbMock.js
@@ -0,0 +1,76 @@
+const user = {
+  "firstName": "Usuário",
+  "lastName": "Teste",
+  "email": "teste@email.com",
+  "password": "123456",
+  "tasks": [
+    {
+      "_id": {
+        "$oid": "620d5ce4006431ef60049daa"
+      },
+      "description": "Fazer almoço",
+      "status": "active"
+    },
+    {
+      "_id": {
+        "$oid": "620d5d82ee59fe8d563526e3"
+      },
+      "description": "Fazer jantar",
+      "status": "active"
+    },
+    {
+      "_id": {
+        "$oid": "620d6e1f357f00ae28e66da5"
+      },
+      "description": "Fazer café da manhã",
+      "status": "active"
+    },
+  ],
+};
+
+const newUser = {
+  firstName: 'Usuário',
+  lastName: 'Teste 02',
+  email: 'teste2@email.com',
+  password: '123456',
+};
+
+const tasksIds = [
+  '620d5ce4006431ef60049daa',
+  '620d5d82ee59fe8d563526e3',
+  '620d6e1f357f00ae28e66da5',
+];
+
+const tasks = [
+  {
+    "_id": {
+      "$oid": "620d5ce4006431ef60049daa"
+    },
+    "description": "Fazer almoço",
+    "status": "active"
+  },
+  {
+    "_id": {
+      "$oid": "620d5d82ee59fe8d563526e3"
+    },
+    "description": "Fazer jantar",
+    "status": "active"
+  },
+  {
+    "_id": {
+      "$oid": "620d6e1f357f00ae28e66da5"
+    },
+    "description": "Fazer café da manhã",
+    "status": "active"
+  },
+];
+
+const email = 'teste@email.com';
+
+module.exports = {
+  tasksIds,
+  email,
+  user,
+  newUser,
+  tasks,
+};
