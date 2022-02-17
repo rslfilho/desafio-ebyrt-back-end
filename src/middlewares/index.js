df008b45af0f824b3d92f9cc21c77ef03e2333b9
Roberval Filho <rslfilho@gmail.com>
2022-02-17 09:14:36 -0300

    feat : configuração do swagger

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const validation = require('./validation');
 const error = require('./error');
 const auth = require('./auth');
+const swagger = require('./swagger');
 
 module.exports = {
   validation,
   error,
   auth,
+  swagger,
 };
