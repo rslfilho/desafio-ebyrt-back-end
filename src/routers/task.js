0d90a6a9434c905acdfe71183d558b212b8dd3d5
Roberval Filho <rslfilho@gmail.com>
2022-02-17 09:05:21 -0300

    feat : router da rota de listar tarefas do usuário

--- src/routers/task.js
+++ src/routers/task.js
@@ -6,6 +6,7 @@ const { validation, auth } = require('../middlewares');
 const router = express.Router({ mergeParams: true });
 
 router.post('/', auth, validation.description, taskController.create);
+router.get('/:userId', auth, taskController.getAllByUser);
 router.patch('/status/:taskId', auth, validation.status, taskController.updateStatus);
 router.patch(
   '/description/:taskId',
